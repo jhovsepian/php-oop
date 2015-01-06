4250f43e4a0554d3c043fbcd91e67d1c7e845cf9
Jacqueline Hovsepian <jhovsepian1018@gmail.com>
2015-01-06 09:13:35 -0800

    made a class ,property ,method.

--- php oop.php
+++ php oop.php
@@ -7,3 +7,18 @@ class Turtle{
 $turtle1 = new Turtle();
 $turtle2 = new Turtle();
 
+classTurtle {
+	public $name="sparkles";
+	public $breed="best breed";
+	public $gender="female";
+	public $price= 5;
+}
+
+$turtle1 = new Turtle();
+print $turtle1->name;
+// sparkles
+
+public function myMethod( $argument, $another) {
+	//stuff
+}
+
