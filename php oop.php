c31f86bbc47e84e2159194579531b4f36fd21616
Jacqueline Hovsepian <jhovsepian1018@gmail.com>
2015-01-07 07:48:30 -0800

    i forgot to push this to git hub yesterday!!!!!!! i did this last night around 8 but now im submitting it

--- php oop.php
+++ php oop.php
@@ -8,7 +8,7 @@ $turtle1 = new Turtle();
 $turtle2 = new Turtle();
 
 classTurtle {
-	public $name="sparkles";
+	public $name="default name";
 	public $breed="best breed";
 	public $gender="female";
 	public $price= 5;
@@ -22,3 +22,112 @@ public function myMethod( $argument, $another) {
 	//stuff
 }
 
+classTurtle {
+	public $firstName = "default name";
+	public $lastName = "default last name";
+	public $gender = "female";
+	public $price = 5;
+
+	function getName() {
+	return "{$this->firstName}"
+	"{$this->lastName}";
+	}
+}
+
+$turtle1 = new Turtle();
+$turtle1->firstName = "Sparkly";
+$turtle1->lastName = "Shiny";
+
+print"The turtle's name is {$turtle1->getName()}.";
+
+//the Turtle's name is Sparkly Shiny.
+
+
+class Snake{
+	//class body
+	//class body
+	//class body
+}
+
+$snake1 = new Snake();
+$snake2 = new Snake();
+
+classSnake {
+	public $name="default name";
+	public $breed="best breed";
+	public $gender="male";
+	public $price= 500;
+}
+
+$snake1 = new Snake();
+print $snake1->name;
+// slither
+
+public function myMethod( $argument, $another) {
+	//stuff
+}
+
+classSnake {
+	public $firstName = "default name";
+	public $lastName = "default last name";
+	public $gender = "male";
+	public $price = 500;
+
+	function getName() {
+	return "{$this->firstName}"
+	"{$this->lastName}";
+	}
+}
+
+$turtle1 = new Snake();
+$turtle1->firstName = "Slither";
+$turtle1->lastName = "Shark";
+
+print"The snake's name is {$snake1->getName()}.";
+
+//the Snake's name is Slither Shark.
+
+
+class Bird{
+	//class body
+	//class body
+	//class body
+}
+
+$bird1 = new Bird();
+$bird2 = new Bird();
+
+classBird {
+	public $name="default name";
+	public $breed="best breed";
+	public $gender="female";
+	public $price= 15;
+}
+
+$bird1 = new Bird();
+print $bird1->name;
+// Chirp Chirp
+
+public function myMethod( $argument, $another) {
+	//stuff
+}
+
+classBird {
+	public $firstName = "default name";
+	public $lastName = "default last name";
+	public $gender = "female";
+	public $price = 15;
+
+	function getName() {
+	return "{$this->firstName}"
+	"{$this->lastName}";
+	}
+}
+
+$bird1 = new Bird();
+$bird1->firstName = "Chirp Chirp";
+$bird1->lastName = "Choo Choo";
+
+print"The bird's name is {$bird1->getName()}.";
+
+//the Bird's name is Chirp Chirp Choo Choo.
